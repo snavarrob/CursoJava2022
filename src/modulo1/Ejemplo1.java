3c6f65d9a3991667fe54773d7a156e3713346767
sergio-navarro <https://sergio-navarro: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@github.developer.allianz.io/sergio-navarro>
2022-05-25 16:41:25 +0200

    Primer Ejemplo

---
+++ src/modulo1/Ejemplo1.java
@@ -0,0 +1,10 @@
+package modulo1;
+
+public class Ejemplo1 {
+
+	public static void main(String[] args) {
+        System.out.println("Hola Sergio");
+        System.out.println("\n\nnombre\tapellido\tlink git");
+        System.out.println("Sergio\t\tNavarro\t\thttps://github.com/snavarrob/CursoJava2022");
+	}
+}
